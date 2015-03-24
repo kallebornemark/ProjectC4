6e260dfa2adbf742067946ca776b6f394219d190
Jimmy <jimmy.maksymiw@gmail.com>
2015-03-24 14:31:50 +0100

    NEW: class GameController, GameGrid

--- Android/C4/app/src/main/java/projectc4/c4/GameController.java
+++ Android/C4/app/src/main/java/projectc4/c4/GameController.java
@@ -5,8 +5,35 @@ package projectc4.c4;
  */
 public class GameController {
     private ClientController clientController;
+    private GameGrid gameGrid;
+    private int[] size = new int[6];
+    private int player;
+    private ClientUI clientUI;
 
     public GameController(ClientController clientController) {
         this.clientController = clientController;
+        gameGrid = new GameGrid();
+        player = 1;
+        clientUI = clientController.getClientUI();
     }
+
+    public void insertTile(int x) {
+        if(size[x] < 7) {
+            gameGrid.setElement((gameGrid.getHeight() - 1) - (size[x]++), x, player);
+            clientUI.drawTile((((gameGrid.getHeight() - 1) - (size[x]-1)) * 6) + (x + 1), player);
+            //Vinstkoll
+            if(player == 1) {
+                player = 2;
+            }else {
+                player = 1;
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+//        GameController con = new GameController();
+    }
+
+
 }
