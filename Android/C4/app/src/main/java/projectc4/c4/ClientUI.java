cfab0be3ed3f6164bdca96e27eca25c9ddd154a0
Emil <emilsandgren94@hotmail.com>
2015-03-24 14:11:04 +0100

    Design och ska börja med lyssnare i clientUI

--- Android/C4/app/src/main/java/projectc4/c4/ClientUI.java
+++ Android/C4/app/src/main/java/projectc4/c4/ClientUI.java
@@ -1,12 +1,37 @@
 package projectc4.c4;
 
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * @author Kalle Bornemark
  */
 public class ClientUI {
     private ClientController clientController;
+    private TextView[][] textViews = new TextView[7][6];
+    private MainActivity mainActivity;
 
     public ClientUI(ClientController clientController) {
         this.clientController = clientController;
+        mainActivity = clientController.getMainActivity();
     }
+
+    public void columnChooser() {
+        ArrayList<Button> buttonArrayList = new ArrayList<>();
+        buttonArrayList = mainActivity.getButtonArrayList();
+
+        buttonArrayList.get(0).setOnClickListener( new Button.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
 }
+
+
