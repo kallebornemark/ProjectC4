c3ca8d4cc009c881c887a43b72476f6f2fa188bf
Erik <eriksandgren94@hotmail.com>
2015-03-31 14:43:28 +0200

    GameController erik ny

--- Android/C4/app/src/main/java/projectc4/c4/client/GameController.java
+++ Android/C4/app/src/main/java/projectc4/c4/client/GameController.java
@@ -1,20 +1,22 @@
 package projectc4.c4.client;
+import java.util.ArrayList;
 
 /**
  * @author Kalle Bornemark
  */
 public class GameController {
     private ClientController clientController;
-    private Game game;
+    private Game gameGrid;
     private int[] size = new int[6];
-    private int player;
+    private int playerToMakeNextMove;
     private int row, col;
     private int playedTiles;
+    private ArrayList winningTiles = new ArrayList();
 
     public GameController(ClientController clientController) {
         this.clientController = clientController;
-        game = new Game();
-        player = 1;
+        gameGrid = new Game();
+        playerToMakeNextMove = 1;
     }
 
     public void newGame() {
@@ -22,26 +24,27 @@ public class GameController {
         for (int i = 0; i < size.length; i++) {
             size[i] = 0;
         }
-        game.reset();
-        player = 1;
+        gameGrid.reset();
+        playerToMakeNextMove = 1;
+        winningTiles.clear();
         /*
         Lokalt
          */
         if(clientController.getUsername() == null) {
-            clientController.setPlayer(player);
+            clientController.setPlayer(playerToMakeNextMove);
         }
     }
 
     public void newMove(int x) {
         //Om nuvarande spelares nummer är lika med ditt spelar-nummer och kollumnen inte är full
-        if(player == clientController.getPlayer() && size[x] < 7) {
-            row = (game.getHeight() - 1) - (size[x]);
+        if(playerToMakeNextMove == clientController.getPlayer() && size[x] < 7) {
+            row = (gameGrid.getHeight() - 1) - (size[x]);
             col = x;
-            game.setElement((game.getHeight() - 1) - (size[x]++), x, player);
-            clientController.drawTile((((game.getHeight() - 1) - (size[x]-1)) * 6) + x, player);
+            gameGrid.setElement((gameGrid.getHeight() - 1) - (size[x]++), x, playerToMakeNextMove);
+            clientController.drawTile((((gameGrid.getHeight() - 1) - (size[x]-1)) * 6) + x, playerToMakeNextMove);
             playedTiles++;
             if (checkHorizontal() || checkVertical() || checkDiagonalRight() || checkDiagonalLeft()) {
-                clientController.winner(player);
+                clientController.winner(playerToMakeNextMove);
                 System.out.println("Winner");
             } else if (playedTiles == 42) {
                 clientController.draw();
@@ -51,41 +54,52 @@ public class GameController {
                 /*
                 Lokalt
                  */
-                 if(clientController.getUsername() == null) {
-                    clientController.setPlayer(player);
-                 }
+                if(clientController.getUsername() == null) {
+                    clientController.setPlayer(playerToMakeNextMove);
+                }
             }
         }
     }
 
     public void changePlayer() {
-        if(player == 1) {
-            player = 2;
+        if(playerToMakeNextMove == 1) {
+            playerToMakeNextMove = 2;
         }else {
-            player = 1;
+            playerToMakeNextMove = 1;
         }
-        clientController.changeHighlightedPlayer(player);
+        clientController.changeHighlightedPlayer(playerToMakeNextMove);
     }
 
     private boolean checkHorizontal() {
         int counter = 1;
-        for (int i = col; i < game.getLength(); i++) {
-            if (i == game.getLength() - 1 || game.getElement(row, i + 1) != player) {
+        for (int i = col; i < gameGrid.getLength(); i++) {
+            if (i == gameGrid.getLength() - 1 || gameGrid.getElement(row, i + 1) != playerToMakeNextMove) {
                 counter = 1;
                 for (int j = i; j >= 0; j--) {
-                    if (j == 0 || game.getElement(row, j - 1) != player) {
+                    if (j == 0 || gameGrid.getElement(row, j - 1) != playerToMakeNextMove) {
                         return false;
                     } else {
                         counter++;
                     }
                     if (counter == 4) {
+                        System.out.println("j: " + j);
+                        for(int n = 0; n < counter; n++) {
+                            winningTiles.add(calculate(row, j - 1) + n);
+                            System.out.print(calculate(row, j - 1) + n);
+                        }
                         return true;
                     }
                 }
             } else {
+
                 counter++;
             }
             if (counter == 4) {
+                System.out.println("i: " + i);
+                for(int n = 0; n < counter; n++) {
+                    winningTiles.add(calculate(row, i + 1) - n);
+                    System.out.print(calculate(row, i + 1) - n);
+                }
                 return true;
             }
         }
@@ -94,12 +108,16 @@ public class GameController {
 
     private boolean checkVertical(){
         int counter = 1;
-        for(int x = row; x < game.getHeight(); x++) {
-            if(x == game.getHeight() - 1 || game.getElement(x + 1, col) != player) {
+        for(int x = row; x < gameGrid.getHeight(); x++) {
+            if(x == gameGrid.getHeight() - 1 || gameGrid.getElement(x + 1, col) != playerToMakeNextMove) {
                 return false;
             } else {
                 counter++;
                 if (counter == 4) {
+                    System.out.println();
+                    for(int i = 0, j= 0; i < counter; i ++,j += 6) {
+                        winningTiles.add(calculate(row,col) + j);
+                    }
                     return true;
                 }
             }
@@ -110,16 +128,21 @@ public class GameController {
 
     private boolean checkDiagonalRight() {
         int counter = 1;
-        for(int i = col,j = row; i < game.getLength() && j < game.getHeight(); i++, j++) {
-            if(i == game.getLength() - 1 || j == game.getHeight() -1 || game.getElement(j + 1, i + 1) != player) {
+        for(int i = col,j = row; i < gameGrid.getLength() && j < gameGrid.getHeight(); i++, j++) {
+            if(i == gameGrid.getLength() - 1 || j == gameGrid.getHeight() -1 || gameGrid.getElement(j + 1, i + 1) != playerToMakeNextMove) {
                 counter = 1;
                 for(int x = i, y = j; x >= 0 && y >= 0; x--, y--) {
-                    if(x == 0 || y == 0 || game.getElement(y - 1, x - 1) != player) {
+                    if(x == 0 || y == 0 || gameGrid.getElement(y - 1, x - 1) != playerToMakeNextMove) {
                         return false;
                     }else {
                         counter++;
                     }
                     if(counter == 4) {
+                        System.out.println();
+                        for(int n = 0; n < counter; n++) {
+                            winningTiles.add(calculate(y - 1,x - 1) + y+1);
+                            System.out.print(((y) * 6) + x - 1 + y+1 + " ");
+                        }
                         return true;
                     }
                 }
@@ -127,6 +150,11 @@ public class GameController {
                 counter++;
             }
             if(counter == 4) {
+                System.out.println();
+                for(int n = 0, m = 0; n < counter; n++, m += j) {
+                    winningTiles.add(((j) * 6) + i + 1 - m+1);
+                    System.out.print(((j) * 6) + i + 1 - m+1 + " ");
+                }
                 return true;
             }
         }
@@ -135,11 +163,11 @@ public class GameController {
 
     private boolean checkDiagonalLeft(){
         int counter = 1;
-        for(int i = col,j = row; i >= 0 && j < game.getHeight(); i--, j++) {
-            if(i == 0 || j == game.getHeight() -1 || game.getElement(j + 1, i - 1) != player) {
+        for(int i = col,j = row; i >= 0 && j < gameGrid.getHeight(); i--, j++) {
+            if(i == 0 || j == gameGrid.getHeight() -1 || gameGrid.getElement(j + 1, i - 1) != playerToMakeNextMove) {
                 counter = 1;
-                for(int x = i, y = j; x < game.getLength() && y >= 0; x++, y--) {
-                    if(x == game.getLength() - 1 || y == 0 || game.getElement(y - 1, x + 1) != player) {
+                for(int x = i, y = j; x < gameGrid.getLength() && y >= 0; x++, y--) {
+                    if(x == gameGrid.getLength() - 1 || y == 0 || gameGrid.getElement(y - 1, x + 1) != playerToMakeNextMove) {
                         return false;
                     }else {
                         counter++;
@@ -154,5 +182,10 @@ public class GameController {
             }
         }
         return false;
+    }
+
+    private int calculate(int row, int col) {
+        int value = (row * 6) + col;
+        return value;
     }
 }
