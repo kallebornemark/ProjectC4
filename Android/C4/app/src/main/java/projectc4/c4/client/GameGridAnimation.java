523d658c1706516c59010b20e1abe00137118bd2
Jimmy <jimmy.maksymiw@gmail.com>
2015-04-15 10:43:24 +0200

    fixad nullpointer i xml-designläget

--- Android/C4/app/src/main/java/projectc4/c4/client/GameGridAnimation.java
+++ Android/C4/app/src/main/java/projectc4/c4/client/GameGridAnimation.java
@@ -135,20 +135,20 @@ public class GameGridAnimation extends View {
     protected void onMeasure(int widthMeasuredSpec, int heightMeasuredSpec) {
         width = MeasureSpec.getSize(widthMeasuredSpec);
         height = MeasureSpec.getSize(heightMeasuredSpec);
+        if (gameController != null) {
+            rows = gameController.getBoardHeight();
+            cols = gameController.getBoardWidth();
 
-        rows = gameController.getBoardHeight();
-        cols = gameController.getBoardWidth();
+            // Räkna ut passande storlek för brickan
+            sideOfTile = Math.min((((width - GRIDSPACING) / gameController.getBoardWidth()) - GRIDSPACING),
+                    (((height - GRIDSPACING) / gameController.getBoardHeight()) - GRIDSPACING));
 
-        // Räkna ut passande storlek för brickan
-        sideOfTile = Math.min((((width - GRIDSPACING) / gameController.getBoardWidth()) - GRIDSPACING),
-                (((height - GRIDSPACING) / gameController.getBoardHeight()) - GRIDSPACING));
+            // Rita gameBoard mitt i canvasen i x-led
+            offsetX = (width - (gameController.getBoardWidth() * (sideOfTile + GRIDSPACING) - GRIDSPACING)) / 2;
 
-        // Rita gameBoard mitt i canvasen i x-led
-        offsetX = (width - (gameController.getBoardWidth() * (sideOfTile + GRIDSPACING) - GRIDSPACING)) / 2;
-
-        // Rita gameBoard längst ner på canvasen i y-led
-        offsetY = (height - (gameController.getBoardHeight() * (sideOfTile + GRIDSPACING)));
-
+            // Rita gameBoard längst ner på canvasen i y-led
+            offsetY = (height - (gameController.getBoardHeight() * (sideOfTile + GRIDSPACING)));
+        }
         System.out.println("GGA - width: " + width + " height: " + height + "\nsideOfTile: " + sideOfTile);
         setMeasuredDimension(width, height);
     }
