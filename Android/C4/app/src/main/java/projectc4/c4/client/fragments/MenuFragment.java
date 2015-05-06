cab9cd34107c2cb1ba430133ab53aaf13003732d
Emil <emilsandgren94@hotmail.com>
2015-05-06 15:09:53 +0200

    gamesettingsfragment()emilbest

--- Android/C4/app/src/main/java/projectc4/c4/client/fragments/MenuFragment.java
+++ Android/C4/app/src/main/java/projectc4/c4/client/fragments/MenuFragment.java
@@ -98,7 +98,7 @@ public class MenuFragment extends Fragment {
                     break;
 
                 case R.id.buttonSettings:
-                    transaction.replace(R.id.activity_layout_fragmentpos, new SettingsFragment()).addToBackStack(null).commit();
+                    transaction.replace(R.id.activity_layout_fragmentpos, new GameSettingsFragment()).addToBackStack(null).commit();
                     break;
 
                 case R.id.buttonHowToPlay:
