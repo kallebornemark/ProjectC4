9854a9540d47ff5b225dce6de36cfbb393e60bba
Erik <eriksandgren94@hotmail.com>
2015-05-06 11:47:24 +0200

    Backlog

--- Android/C4/app/src/main/java/projectc4/c4/client/fragments/MenuFragment.java
+++ Android/C4/app/src/main/java/projectc4/c4/client/fragments/MenuFragment.java
@@ -80,13 +80,13 @@ public class MenuFragment extends Fragment {
             switch (v.getId()) {
                 case R.id.buttonPlayLocal:
                     ((MainActivity)getActivity()).getClientController().setGameMode(C4Constants.LOCAL);
-                    transaction.replace(R.id.activity_layout_fragmentpos, new GameFragment()).addToBackStack(null).commit();
+                    transaction.replace(R.id.activity_layout_fragmentpos, new GameFragment()).addToBackStack("Local").commit();
                     break;
 
                 case R.id.buttonPlayOnline:
                     if ((((MainActivity)getActivity()).getClientController().getClient().getUser() != null)) {
                         System.out.println("Ändrar till matchmaking");
-                        transaction.replace(R.id.activity_layout_fragmentpos, new MatchmakingFragment()).addToBackStack(null).commit();
+                        transaction.replace(R.id.activity_layout_fragmentpos, new MatchmakingFragment()).addToBackStack("Matchmaking").commit();
                     } else {
                         System.out.println("Ändrar till Login");
                         Socket socket = ((MainActivity)getActivity()).getClientController().getClient().getSocket();
